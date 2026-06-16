eba3ce8aa48b5b8e0f3378b117205de4188912fb
ofaith@byupathway.edu <ofaith@byupathway.edu>
2026-06-16 16:59:29 +0100

    host

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.MapStaticAssets();
-app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseStatusCodePagesWithReExecute("/not-found");
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
